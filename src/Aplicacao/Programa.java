bf1c9290291b8261013d43b984f07c0e00ae90e4
V7ctor <joaovictor89437@gmail.com>
2019-10-31 13:27:07 -0300

    Classe Departamento Implementada

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,9 +1,12 @@
 package Aplicacao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import BD.Conexao;
 import BD.DBExcecaoIntegracao;
+import Entidades.Departamento;
 
 public class Programa {
 
@@ -14,7 +17,18 @@ public class Programa {
 		try {
 			
 			con = Conexao.getConexao();
+		    
+			List<Departamento> dep = new ArrayList<>();
 			
+			dep.add(new Departamento(1, "Livros"));
+			dep.add(new Departamento(2, "Bibliotecas"));
+			dep.add(new Departamento(3, "Faxinaria"));
+			dep.add(new Departamento(4, "Administracao"));
+			dep.add(new Departamento(5, "Programação"));
+			
+			for (Departamento d : dep) {
+				System.out.println(d.toString());
+			}
 			
 		} catch (RuntimeException e) {
 			throw new DBExcecaoIntegracao(e.getMessage());
